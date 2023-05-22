1fec6068654076c0d67896ce92a5787d26b84bd8
Vijaya Challa <vijayajay1814@gmail.com>
2023-05-22 13:05:59 +0200

    remove navbar on landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,47 +13,118 @@ import Missing from 'auth/components/Missing';
 import TaskSection from 'gameSection/TaskSection/TaskSection';
 import EditProfile from 'auth/components/EditProfile';
 import DeleteAccount from 'auth/components/DeleteAccount';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
+import { createContext, useState, useMemo } from 'react';
 import Admin from './auth/components/admin/Admin';
 import Tasks from './auth/components/admin/Tasks';
 import Levels from './auth/components/admin/Levels';
 import Home from './components/Home/Home';
 import HowPlay from './components/HowPlay/HowPlay';
 
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
 function App() {
+  const [mode, setMode] = useState('dark');
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      }
+    }),
+    []
+  );
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          ...(mode === 'light'
+            ? {
+                // palette values for light mode
+                background: {
+                  paper: '#95b7b7',
+                  default: '#b3cbcb'
+                },
+                primary: {
+                  // light: '#757ce8',
+                  main: '#5c8a8a'
+                  // dark: '#002884',
+                  // contrastText: '#fff'
+                }
+                // secondary: {
+                //   light: '#ff7961',
+                //   main: '#f44336',
+                //   dark: '#ba000d',
+                //   contrastText: '#000'
+                // }
+              }
+            : {
+                // palette values for dark mode
+                background: {
+                  paper: '#424242',
+                  default: '#212121'
+                }
+                // primary: {
+                //   light: '#757ce8',
+                //   main: '#3f50b5',
+                //   dark: '#002884',
+                //   contrastText: '#fff'
+                // },
+                // secondary: {
+                //   light: '#ff7961',
+                //   main: '#f44336',
+                //   dark: '#ba000d',
+                //   contrastText: '#000'
+                // }
+              }),
+          contrastThreshold: 4.5
+        }
+      }),
+    [mode]
+  );
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* PUBLIC routes */}
-        <Route path="landingpage" element={<Home />} />
-        <Route path="login" element={<SignIn />} />
-        <Route path="howtoplay" element={<HowPlay />} />
-        <Route path="register" element={<SignUp />} />
-        <Route path="unauthorized" element={<Unauthorized />} />
-        <Route path="tasksection" element={<TaskSection />} />
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SnackbarProvider maxSnack={5}>
+          <Routes>
+            <Route path="landingpage" element={<Home />} />
+            <Route path="/" element={<Layout colorModeContext={ColorModeContext} />}>
+              {/* PUBLIC routes */}
+              <Route path="login" element={<SignIn />} />
+              <Route path="howtoplay" element={<HowPlay />} />
+              <Route path="register" element={<SignUp />} />
+              <Route path="unauthorized" element={<Unauthorized />} />
+              <Route path="tasksection" element={<TaskSection />} />
 
-        {/* PROTECTED routes */}
+              {/* PROTECTED routes */}
 
-        {/* USER + ADMIN */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.User, ROLES.Admin]} />}>
-          <Route path="/" element={<AuthDetails />} />
-          <Route path="/editprofile" element={<EditProfile />} />
-          <Route path="/tasksection" element={<TaskSection />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/deleteprofile" element={<DeleteAccount />} />
-        </Route>
+              {/* USER + ADMIN */}
+              <Route element={<RequireAuth allowedRoles={[ROLES.User, ROLES.Admin]} />}>
+                <Route path="/" element={<AuthDetails />} />
+                <Route path="/editprofile" element={<EditProfile />} />
+                <Route path="/tasksection" element={<TaskSection />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/deleteprofile" element={<DeleteAccount />} />
+              </Route>
 
-        {/* ADMIN */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/users" element={<Users />} />
-          <Route path="/admin/levels" element={<Levels />} />
-          <Route path="/admin/tasks" element={<Tasks />} />
-        </Route>
+              {/* ADMIN */}
+              <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/admin/users" element={<Users />} />
+                <Route path="/admin/levels" element={<Levels />} />
+                <Route path="/admin/tasks" element={<Tasks />} />
+              </Route>
 
-        {/* CATCH MISSING */}
-        <Route path="*" element={<Missing />} />
-      </Route>
-    </Routes>
+              {/* CATCH MISSING */}
+              <Route path="*" element={<Missing />} />
+            </Route>
+          </Routes>
+        </SnackbarProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- client/src/auth/components/Layout.jsx
+++ client/src/auth/components/Layout.jsx
@@ -1,93 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from 'common/components/Navbar';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import { SnackbarProvider } from 'notistack';
-import { createContext, useState, useMemo } from 'react';
 
-// const darkTheme = createTheme({
-//   palette: { mode: 'light' }
-// });
+function Layout(props) {
+  const { colorModeContext } = props;
 
-// const lightTheme = createTheme({
-//   palette: { mode: 'light' }
-// });
-
-const ColorModeContext = createContext({ toggleColorMode: () => {} });
-
-function Layout() {
-  const [mode, setMode] = useState('dark');
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      }
-    }),
-    []
-  );
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-          ...(mode === 'light'
-            ? {
-                // palette values for light mode
-                background: {
-                  paper: '#95b7b7',
-                  default: '#b3cbcb'
-                },
-                primary: {
-                  // light: '#757ce8',
-                  main: '#5c8a8a'
-                  // dark: '#002884',
-                  // contrastText: '#fff'
-                }
-                // secondary: {
-                //   light: '#ff7961',
-                //   main: '#f44336',
-                //   dark: '#ba000d',
-                //   contrastText: '#000'
-                // }
-              }
-            : {
-                // palette values for dark mode
-                background: {
-                  paper: '#424242',
-                  default: '#212121'
-                }
-                // primary: {
-                //   light: '#757ce8',
-                //   main: '#3f50b5',
-                //   dark: '#002884',
-                //   contrastText: '#fff'
-                // },
-                // secondary: {
-                //   light: '#ff7961',
-                //   main: '#f44336',
-                //   dark: '#ba000d',
-                //   contrastText: '#000'
-                // }
-              }),
-          contrastThreshold: 4.5
-        }
-      }),
-    [mode]
-  );
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <SnackbarProvider maxSnack={5}>
-          <main className="App">
-            <Navbar colorModeContext={ColorModeContext} />
-            <div className="centerBox">
-              <Outlet />
-            </div>
-          </main>
-        </SnackbarProvider>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <main className="App">
+      <Navbar colorModeContext={colorModeContext} />
+      <div className="centerBox">
+        <Outlet />
+      </div>
+    </main>
   );
 }
 
